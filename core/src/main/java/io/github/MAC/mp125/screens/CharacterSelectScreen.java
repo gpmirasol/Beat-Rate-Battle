d9e760bb667f6ce13e564672343f59bc4176c03e
Grace Angela Mirasol <gpmirasol@up.edu.ph>
2026-05-21 02:53:31 +0800

    Added basic layout buttons, labels, through table for Char Select Screen

--- core/src/main/java/io/github/MAC/mp125/screens/CharacterSelectScreen.java
+++ core/src/main/java/io/github/MAC/mp125/screens/CharacterSelectScreen.java
@@ -4,12 +4,37 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class CharacterSelectScreen implements Screen {
     private Game game;
+
     private Stage stage;
+    private Skin skin;
+
+    private Table rootTable;
+
+    // preview placeholders
+    private Label p1Preview;
+    private Label p2Preview;
+
+    // character buttons
+    private TextButton p1Char1Button;
+    private TextButton p1Char2Button;
+
+    private TextButton p2Char1Button;
+    private TextButton p2Char2Button;
+
+    // get ready for battle banner
+    private Label battleBanner;
 
     public CharacterSelectScreen(Game game) {
         this.game = game;
@@ -17,16 +42,116 @@ public class CharacterSelectScreen implements Screen {
 
     @Override
     public void show() {
+
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        rootTable = new Table();
+        rootTable.setFillParent(true);
+
+        stage.addActor(rootTable);
+
+        // top preview area
+        p1Preview = new Label("P1 PREVIEW", skin);
+        p2Preview = new Label("P2 PREVIEW", skin);
+
+        p1Preview.setAlignment(Align.center);
+        p2Preview.setAlignment(Align.center);
+
+        // character buttons - player 1
+        p1Char1Button = new TextButton("P1 Char 1", skin);
+        p1Char2Button = new TextButton("P1 Char 2", skin);
+
+        // character buttons - player 2
+        p2Char1Button = new TextButton("P2 Char 1", skin);
+        p2Char2Button = new TextButton("P2 Char 2", skin);
+
+        // player 1 button area - left side
+        Table p1ButtonsTable = new Table();
+
+        p1ButtonsTable.add(p1Char1Button)
+            .width(180)
+            .height(70)
+            .pad(10);
+
+        p1ButtonsTable.add(p1Char2Button)
+            .width(180)
+            .height(70)
+            .pad(10);
+
+        // player 2 button area - left side
+        Table p2ButtonsTable = new Table();
+
+        p2ButtonsTable.add(p2Char1Button)
+            .width(180)
+            .height(70)
+            .pad(10);
+
+        p2ButtonsTable.add(p2Char2Button)
+            .width(180)
+            .height(70)
+            .pad(10);
+
+        // battle banner
+        battleBanner = new Label("GET READY FOR BATTLE", skin);
+        battleBanner.setVisible(false); // set visible true in logic part upon pressing "enter"
+        battleBanner.setAlignment(Align.center);
+
+        // MAIN LAYOUT
+        // TOP PREVIEW ROW
+        rootTable.add(p1Preview)
+            .expandX()
+            .center()
+            .padTop(50);
+
+        rootTable.add(p2Preview)
+            .expandX()
+            .center()
+            .padTop(50);
+
+        rootTable.row();
+
+        // EMPTY SPACE
+        rootTable.add().height(300);
+        rootTable.add();
+
+        rootTable.row();
+
+        // BOTTOM BUTTON ROW
+        rootTable.add(p1ButtonsTable)
+            .expandX()
+            .left()
+            .padLeft(50)
+            .padBottom(50);
+
+        rootTable.add(p2ButtonsTable)
+            .expandX()
+            .right()
+            .padRight(50)
+            .padBottom(50);
+
+        rootTable.row();
+
+        // BATTLE BANNER
+        rootTable.add(battleBanner)
+            .colspan(2)
+            .center()
+            .padBottom(20);
+
 
         System.out.println("Character Select Screen");
     }
 
     @Override
     public void render(float delta) {
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -45,5 +170,8 @@ public class CharacterSelectScreen implements Screen {
     public void hide() {}
 
     @Override
-    public void dispose() {}
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
 }

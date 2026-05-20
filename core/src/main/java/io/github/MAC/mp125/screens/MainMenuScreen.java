0af760a48129bd941c256018d86726f0ef319720
Grace Angela Mirasol <gpmirasol@up.edu.ph>
2026-05-20 23:30:18 +0800

    Fixed draft layout using table, added bg placeholder, and replaced text with image

--- core/src/main/java/io/github/MAC/mp125/screens/MainMenuScreen.java
+++ core/src/main/java/io/github/MAC/mp125/screens/MainMenuScreen.java
@@ -2,12 +2,16 @@ package io.github.MAC.mp125.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -15,7 +19,12 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 /** First screen of the application. Displayed after the application is created. */
 public class MainMenuScreen implements Screen {
     private Stage stage;
-    private TextButton playButton;
+    private Table table;
+    private Image background;
+
+    private ImageButton playButton;
+    private ImageButton howToPlayButton;
+
     private Skin skin;
     private Game game;
 
@@ -25,20 +34,29 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-        // Prepare your screen here.
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
+        Texture bgTexture = new Texture(Gdx.files.internal("bg.png"));
+        background = new Image(bgTexture);
+        background.setFillParent(true);
+        stage.addActor(background);
+
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        table.center();
+
         skin = new Skin(Gdx.files.internal("uiskin.json"));
-        playButton = new TextButton("Play", skin);
 
-        playButton.setPosition(
-            Gdx.graphics.getWidth() / 2f - 100,
-            Gdx.graphics.getHeight() / 2f
-        );
+        playButton = new ImageButton(skin);
+        howToPlayButton = new ImageButton(skin);
 
-        stage.addActor(playButton);
+        table.add(playButton).width(250).height(80).pad(10);
+        table.row();
+        table.add(howToPlayButton).width(250).height(80);
 
         playButton.addListener(new ClickListener() {
             @Override
@@ -64,14 +82,10 @@ public class MainMenuScreen implements Screen {
     }
 
     @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
+    public void resume() {}
 
     @Override
     public void hide() {

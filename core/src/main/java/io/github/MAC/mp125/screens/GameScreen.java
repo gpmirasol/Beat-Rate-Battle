a697a2b0e547c0ef33908c62916aa8a06fe4e1be
Grace Angela Mirasol <gpmirasol@up.edu.ph>
2026-05-20 22:18:48 +0800

    Added other 3 screens with basic layout

---
+++ core/src/main/java/io/github/MAC/mp125/screens/GameScreen.java
@@ -0,0 +1,44 @@
+package io.github.MAC.mp125.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+public class GameScreen implements Screen {
+    private Game game;
+    public GameScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Song Select Screen");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
